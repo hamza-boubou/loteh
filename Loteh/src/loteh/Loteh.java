9149e19b2fa14aef42c3d4bb5acbd2d2552d9d44
Hamza Boubou <48151385+hamza-boubou@users.noreply.github.com>
2023-11-24 17:01:47 +0100

    Añadido clases vista y modelo

--- Loteh/src/loteh/Loteh.java
+++ Loteh/src/loteh/Loteh.java
@@ -4,6 +4,10 @@ public class Loteh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Modelo mi_objeto_modelo= new Modelo();
+		Vista mi_objeto_vista= new Vista();
+		Controlador el_controlador= new Controlador(mi_objeto_modelo,mi_objeto_vista);
+		el_controlador.iniciarVista();S
 
 	}
 
---
+++ Loteh/src/loteh/Modelo.java
@@ -0,0 +1,5 @@
+package loteh;
+
+public class Modelo {
+
+}
